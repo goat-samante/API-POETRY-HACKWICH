6c7e4a76f6ee7e36a485296e8ba693c5f1276e72
Samante <samante@bsd220.org>
2025-04-13 18:47:11 -0500

    finish setting up alert

--- API POETRY HACKWICH/ContentView.swift
+++ API POETRY HACKWICH/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var authors = [String]()
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView {
             List(authors, id: \.self) { author in
@@ -20,7 +23,12 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
         .task {
-            await getAuthors()  
+            await getAuthors()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the poetry authors"),
+                  dismissButton: .default(Text("Ok")))
         }
     }
     func getAuthors() async {
@@ -29,9 +37,11 @@ struct ContentView: View {
                if let (data, _) = try? await URLSession.shared.data(from: url) {
                    if let decodedResponse = try? JSONDecoder().decode(Authors.self, from: data) {
                        authors = decodedResponse.authors
+                       return
                    }
                }
            }
+        showingAlert = true
        }
 }
 struct Authors: Codable {
