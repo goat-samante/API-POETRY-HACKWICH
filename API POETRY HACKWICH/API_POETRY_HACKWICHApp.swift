e4f41d99ecefdcaab4fba526bfee6349133f2611
Samante <samante@bsd220.org>
2025-04-13 18:25:57 -0500

    Initial Commit

---
+++ API POETRY HACKWICH/API_POETRY_HACKWICHApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_POETRY_HACKWICHApp.swift
+//  API POETRY HACKWICH
+//
+//  Created by Samuel Amante on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct API_POETRY_HACKWICHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API POETRY HACKWICHTests/API_POETRY_HACKWICHTests.swift
@@ -0,0 +1,17 @@
+//
+//  API_POETRY_HACKWICHTests.swift
+//  API POETRY HACKWICHTests
+//
+//  Created by Samuel Amante on 4/13/25.
+//
+
+import Testing
+@testable import API_POETRY_HACKWICH
+
+struct API_POETRY_HACKWICHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
